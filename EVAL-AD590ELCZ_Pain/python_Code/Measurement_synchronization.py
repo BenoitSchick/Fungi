d93975f2f2e2c9da104b0172db97f6466a895281
raspberry <raspberrypi@gmail.com>
2026-03-27 16:02:54 +0100

    update measurement_synchronisation.py

--- EVAL-AD590ELCZ_Pain/python_Code/Measurement_synchronization.py
+++ EVAL-AD590ELCZ_Pain/python_Code/Measurement_synchronization.py
@@ -16,25 +16,30 @@ GPIO.setup(led37, GPIO.OUT)
 
 while True:
 
-    #GPIO.output(led36, True)
-    #time.sleep(0.2)
-    #GPIO.output(led36, False)
-    #time.sleep(5)
+    GPIO.output(led36, True)
+    time.sleep(0.2)
+    GPIO.output(led36, False)
+    time.sleep(5)
     
-    #GPIO.output(led37, True)
-    #time.sleep(0.2)
-    #GPIO.output(led37, False)
-    #time.sleep(5)
+    GPIO.output(led37, True)
+    time.sleep(0.2)
+    GPIO.output(led37, False)
+    time.sleep(5)
 
     GPIO.output(led38, True)
     time.sleep(0.2)
     GPIO.output(led38, False)
     time.sleep(5)
 
-    #GPIO.output(led40, True)
-    #time.sleep(0.2)
-    #GPIO.output(led40, False)
-    #time.sleep(5)
+    GPIO.output(led40, True)
+    time.sleep(0.2)
+    GPIO.output(led40, False)
+    
+
+    time.sleep(300)
+
+    
+
 
 
 
