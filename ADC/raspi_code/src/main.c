f6de709a2861fda8e5d502da90b04d6cc4abceb5
benoit schick <benoitschick19@gmail.com>
2026-04-20 17:56:22 +0200

    feat: add AD777x driver skeleton for Raspberry Pi Pico

--- ADC/raspi_code/src/main.c
+++ ADC/raspi_code/src/main.c
@@ -1,202 +1,128 @@
-#include <stdio.h>
+/***************************************************************************//**
+ *   @file   main.c
+ *   @brief  Main function
+ *   @author Benoit Schick (benoit.schick@hefr.ch)
+********************************************************************************/
+
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
-
-#define CS_PIN 7 
-#define SCK_PIN 4
-#define MOSI_PIN 5
-#define MISO_PIN 6
-
-// Registers
-static const uint8_t REG_DEVID = 0x00;
-static const uint8_t REG_POWER_CTL = 0x2D;
-static const uint8_t REG_DATAX0 = 0x32;
-
-// Other constants
-static const uint8_t DEVID = 0xE5;
-static const float SENSITIVITY_2G = 1.0 / 256;  // (g/LSB)
-static const float EARTH_GRAVITY = 9.80665;     // Earth's gravity in [m/s^2]
+#include "no-OS/drivers/platform/pico/pico_spi.h"
+#include "no-OS/include/no_os_spi.h"
+#include "no-OS/drivers/adc/ad7779/ad7779.h"
 
 
+// --- Pinout Raspberry Pi -----------------------------
+#define GPIO_RESET 0
+#define GPIO_MODE0 1
+#define GPIO_MODE1 2
+#define GPIO_MODE2 3
+#define GPIO_MODE3 4
+#define GPIO_DCLK0 5
+#define GPIO_DCLK1 6
+#define GPIO_DCLK2 7
+#define GPIO_SYNC_IN 8
+#define GPIO_CONVST_SAR 9
 
-/* #include "pico_spi.h" */
-/* #include "no_os_spi.h" */
-
-/* struct no_os_spi_desc *spi_desc; */
-
-/* // Paramètres spécifiques au Pico (broches) */
-/* struct pico_spi_init_param pico_spi_extra = { */
-/*     .spi_tx_pin  = SPI0_TX_GP19,   // MOSI */
-/*     .spi_rx_pin  = SPI0_RX_GP16,   // MISO */
-/*     .spi_sck_pin = SPI0_SCK_GP18,  // CLK */
-/*     .spi_cs_pin  = SPI0_CS_GP17,   // CS */
-/* }; */
-
-/* // Paramètres généraux SPI */
-/* struct no_os_spi_init_param spi_param = { */
-/*     .device_id     = 0,              // spi0 */
-/*     .max_speed_hz  = 1000000,        // 1 MHz */
-/*     .mode          = NO_OS_SPI_MODE_3, // AD7777 : CPOL=1, CPHA=1 */
-/*     .extra         = &pico_spi_extra, */
-/*     .platform_ops  = &pico_spi_ops, */
-/* }; */
+// --- Registers -----------------------------
+/* static const uint8_t REG_DEVID = 0x00; */
+/* static const uint8_t REG_POWER_CTL = 0x2D; */
+/* static const uint8_t REG_DATAX0 = 0x32; */
 
 
-/*******************************************************************************
- * Function Declarations
- */
-void reg_write( spi_inst_t *spi, 
-                const uint cs, 
-                const uint8_t reg, 
-                const uint8_t data);
 
-int reg_read(  spi_inst_t *spi,
-                const uint cs,
-                const uint8_t reg,
-                uint8_t *buf,
-                uint8_t nbytes);
+/* struct no_os_spi_desc *pico_spi_desc; */
 
-/*******************************************************************************
- * Function Definitions
- */
+// --- Pico SPI Settings -----------------------------
+struct pico_spi_init_param pico_spi_pin = {
+    .spi_tx_pin  = SPI0_TX_GP19,   // MOSI
+    .spi_rx_pin  = SPI0_RX_GP16,   // MISO
+    .spi_sck_pin = SPI0_SCK_GP18,  // CLK
+    .spi_cs_pin  = SPI0_CS_GP17,   // CS
+};
 
-// Write 1 byte to the specified register
-void reg_write( spi_inst_t *spi, 
-                const uint cs, 
-                const uint8_t reg, 
-                const uint8_t data) {
+// --- Pico GPIO Settings -----------------------------
+struct no_os_gpio_init_param gpio_reset_ip =
+        { .number = GPIO_RESET, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_mode0_ip =
+        { .number = GPIO_MODE0, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_mode1_ip =
+        { .number = GPIO_MODE1, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_mode2_ip =
+        { .number = GPIO_MODE2, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_mode3_ip =
+        { .number = GPIO_MODE3, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_dclk0_ip =
+        { .number = GPIO_DCLK0, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_dclk1_ip =
+        { .number = GPIO_DCLK1, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_dclk2_ip =
+        { .number = GPIO_DCLK2, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_sync_in_ip =
+        { .number = GPIO_SYNC_IN, .platform_ops = &pico_gpio_ops, .extra = NULL };
+struct no_os_gpio_init_param gpio_convst_sar_ip =
+        { .number = GPIO_CONVST_SAR, .platform_ops = &pico_gpio_ops, .extra = NULL };
 
-    uint8_t msg[2];
-                
-    // Construct message (set ~W bit low, MB bit low)
-    msg[0] = 0x00 | reg;
-    msg[1] = data;
+// --- General SPI settings -----------------------------
+struct no_os_spi_init_param spi_init_param = {
+    .device_id     		= 0,              // spi0
+    .max_speed_hz  		= 1000000,        // 1 MHz
+    /* .chip_select 		= , */
+    .mode         		= NO_OS_SPI_MODE_0, // AD7777 : CPOL=1, CPHA=1
+    .bit_order 			= NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+    .lanes 			= NO_OS_SPI_SINGLE_LANE,
+    .extra         		= &pico_spi_pin,
+    .platform_ops  		= &pico_spi_ops,
+};
 
-    // Write to register
-    gpio_put(cs, 0);
-    spi_write_blocking(spi, msg, 2);
-    gpio_put(cs, 1);
-}
+// --- ADC AD7779 settings -----------------------------
+ad7779_dev* adc_dev; 
 
-// Read byte(s) from specified register. If nbytes > 1, read from consecutive
-// registers.
-int reg_read(  spi_inst_t *spi,
-                const uint cs,
-                const uint8_t reg,
-                uint8_t *buf,
-                const uint8_t nbytes) {
+ad7779_init_param adc_init_param = {
+	.spi_init = spi_init_param;
+	.gpio_reset      = gpio_reset_ip,
+        .gpio_mode0      = gpio_mode0_ip,
+        .gpio_mode1      = gpio_mode1_ip,
+        .gpio_mode2      = gpio_mode2_ip,
+        .gpio_mode3      = gpio_mode3_ip,
+        .gpio_dclk0      = gpio_dclk0_ip,
+        .gpio_dclk1      = gpio_dclk1_ip,
+        .gpio_dclk2      = gpio_dclk2_ip,
+        .gpio_sync_in    = gpio_sync_in_ip,
+        .gpio_convst_sar = gpio_convst_sar_ip,
+	.ctrl_mode       = AD7779_SPI_CTRL, //Pin or SPI control mode
+        .spi_crc_en      = AD7779_DISABLE,
+        .read_from_cache = true,
+        .state = {
+            AD7779_ENABLE, AD7779_ENABLE, AD7779_ENABLE, AD7779_ENABLE,
+            AD7779_ENABLE, AD7779_ENABLE, AD7779_ENABLE, AD7779_ENABLE
+        },
+        .gain = {
+            AD7779_GAIN_1, AD7779_GAIN_1, AD7779_GAIN_1, AD7779_GAIN_1,
+            AD7779_GAIN_1, AD7779_GAIN_1, AD7779_GAIN_1, AD7779_GAIN_1
+        },
+        .dec_rate_int = 512, //decimation rate
+        .dec_rate_dec = 0,
+        .ref_type     = AD7779_EXT_REF,
+        .pwr_mode     = AD7779_HIGH_RES,
+        .dclk_div     = AD7779_DCLK_DIV_1,
+};
 
-    int num_bytes_read = 0;
-    uint8_t mb = 0;
+int main()
+{
+	//init Raspberry Pi Pico
+	/* int ret1 = pico_spi_init(&pico_spi_desc, &pico_spi_init_param); */
+	
+	//init ADC AD7779
+	int ret = ad7779_init(&adc_dev, adc_init_param);
+	if (ret != 0) {return -1;}
 
-    // Determine if multiple byte (MB) bit should be set
-    if (nbytes < 1) {
-        return -1;
-    } else if (nbytes == 1) {
-        mb = 0;
-    } else {
-        mb = 1;
-    }
 
-    // Construct message (set ~W bit high)
-    uint8_t msg = 0x80 | (mb << 6) | reg;
+	uint8_t reg_val;
+    	ad7779_spi_int_reg_read(dev, AD7779_REG_GENERAL_USER_CONFIG_1, &reg_val);
+    	printf("GENERAL_USER_CONFIG_1 = 0x%02X (attendu 0x60)\n", reg_val);
 
-    // Read from register
-    gpio_put(cs, 0);
-    spi_write_blocking(spi, &msg, 1);
-    num_bytes_read = spi_read_blocking(spi, 0, buf, nbytes);
-    gpio_put(cs, 1);
+	ad7779_set_spi_op_mode(dev, AD7779_SD_CONV); //Switch to sigma-delta acquisition mode
 
-    return num_bytes_read;
-}
-
-/*******************************************************************************
- * Main
- */
-int main() {
-
-    /* int16_t acc_x; */
-    /* int16_t acc_y; */
-    /* int16_t acc_z; */
-    /* float acc_x_f; */
-    /* float acc_y_f; */
-    /* float acc_z_f; */
-
-    // Pins
-
-    // Buffer to store raw reads
-    uint8_t data[6];
-
-    // Ports
-    spi_inst_t *spi = spi0;
-
-    // Initialize chosen serial port
-    stdio_init_all();
-
-    // Initialize CS pin high
-    gpio_init(CS_PIN);
-    gpio_set_dir(CS_PIN, GPIO_OUT);
-    gpio_put(CS_PIN, 1);
-
-    // Initialize SPI port at 1 MHz
-    spi_init(spi, 1000 * 1000);
-
-    // Set SPI format
-    spi_set_format( spi0,   // SPI instance
-                    8,      // Number of bits per transfer
-                    1,      // Polarity (CPOL)
-                    1,      // Phase (CPHA)
-                    SPI_MSB_FIRST);
-
-    // Initialize SPI pins
-    gpio_set_function(SCK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(MOSI_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(MISO_PIN, GPIO_FUNC_SPI);
-
-    /* // Workaround: perform throw-away read to make SCK idle high */
-    /* reg_read(spi, CS_PIN, REG_DEVID, data, 1); */
-
-    /* // Read device ID to make sure that we can communicate with the ADXL343 */
-    /* reg_read(spi, CS_PIN, REG_DEVID, data, 1); */
-    /* if (data[0] != DEVID) { */
-    /*     printf("ERROR: Could not communicate \r\n"); */
-    /*     while (true); */
-    /* } */
-    
-    /* // Read Power Control register */
-    /* reg_read(spi, CS_PIN, REG_POWER_CTL, data, 1); */
-    /* printf("0x␂X\r\n", data[0]); */
-
-    /* // Tell ADXL343 to start taking measurements by setting Measure bit to high */
-    /* data[0] |= (1 << 3); */
-    /* reg_write(spi, cs_pin, REG_POWER_CTL, data[0]); */
-
-    /* // Test: read Power Control register back to make sure Measure bit was set */
-    /* reg_read(spi, cs_pin, REG_POWER_CTL, data, 1); */
-    /* printf("0x␂X\r\n", data[0]); */
-
-    /* // Wait before taking measurements */
-    /* sleep_ms(2000); */
-
-    // Loop forever
-    while (true) {
-
-        /* // Read X, Y, and Z values from registers (16 bits each) */
-        /* reg_read(spi, cs_pin, REG_DATAX0, data, 6); */
-
-        /* // Convert 2 bytes (little-endian) into 16-bit integer (signed) */
-        /* acc_x = (int16_t)((data[1] << 8) | data[0]); */
-        /* acc_y = (int16_t)((data[3] << 8) | data[2]); */
-        /* acc_z = (int16_t)((data[5] << 8) | data[4]); */
-
-        /* // Convert measurements to [m/s^2] */
-        /* acc_x_f = acc_x * SENSITIVITY_2G * EARTH_GRAVITY; */
-        /* acc_y_f = acc_y * SENSITIVITY_2G * EARTH_GRAVITY; */
-        /* acc_z_f = acc_z * SENSITIVITY_2G * EARTH_GRAVITY; */
-
-        /* // Print results */
-        /* printf("X: %.2f | Y: %.2f | Z: %.2f\r\n", acc_x_f, acc_y_f, acc_z_f); */
-
-        /* sleep_ms(100); */
-    }
+	ad7779_remove(adc_dev)
+	return 0;
 }
