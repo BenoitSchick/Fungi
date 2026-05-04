a195b03e546dfb3293fcb1f5569ddcfcec2077b3
BenoitSchick <benoitschick19@gmail.com>
2026-05-04 15:09:25 +0200

    Update test.py

--- ADC/AD777_code/test.py
+++ ADC/AD777_code/test.py
@@ -1,6 +1,29 @@
 import numpy as np
+import time
 
-# data = np.fromfile("Measurement_ADC/channel0", dtype=np.int32)
-data = np.fromfile("Measurement_ADC/timestamp", dtype=np.int32)
+SAMPLES_PER_TIMESTAMP = 500
+values = np.fromfile("Measurement_ADC/channel0", dtype=np.int32)
+timestamps = np.fromfile("Measurement_ADC/timestamp", dtype=np.int64)
 
-print(data[:20])  # afficher les 20 premières valeurs
+print(f"Valeurs lues      : {len(values)}")
+print(f"Timestamps lus    : {len(timestamps)}")
+
+
+nblocks = min(len(timestamps), len(values)
+
+for b in range(nblocks):
+
+    ts = timestamps[b]
+
+    date_str = time.strftime(
+        "%Y-%m-%d %H:%M:%S",
+        time.localtime(ts)
+    )
+
+    start = b * SAMPLES_PER_TIMESTAMP
+    end   = start + SAMPLES_PER_TIMESTAMP
+
+    bloc = values[start:end]
+
+    print(f"=== {date_str} ===")
+    print(bloc)
