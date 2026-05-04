3b96dec12bbda839509599199f2457e6bcfd8e93
rasp <rasp@gmail.com>
2026-05-04 14:57:24 +0200

    add folder Measurement_ADC

--- ADC/AD777_code/test.py
+++ ADC/AD777_code/test.py
@@ -1,5 +1,6 @@
 import numpy as np
 
-data = np.fromfile("channel0", dtype=np.int32)
+# data = np.fromfile("Measurement_ADC/channel0", dtype=np.int32)
+data = np.fromfile("Measurement_ADC/timestamp", dtype=np.int32)
 
 print(data[:20])  # afficher les 20 premières valeurs
