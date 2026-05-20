42174e0972129f850dcf6b4399c2ba0cdb4085af
benoit schick <benoitschick19@gmail.com>
2026-05-20 18:14:18 +0200

    add files

--- ADC/AD777_code/read_binaryData.py
+++ ADC/AD777_code/read_binaryData.py
@@ -2,14 +2,15 @@ import numpy as np
 import time
 
 SAMPLES_PER_TIMESTAMP = 125 
-values = np.fromfile("Measurement_ADC/channel1", dtype=np.int32)
+values = np.fromfile("Measurement_ADC/channel2", dtype=np.int32)
 timestamps = np.fromfile("Measurement_ADC/timestamp", dtype=np.int64)
 
 n = len(values)
 
 for i in range(n):
     date_str = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(timestamps[i]))
-    print(f"Date: {date_str} | Valeur: {values[i]}")
+    # print(f"Date: {date_str} | Valeur: {values[i]}")
+    print(f"{values[i]}")
 
 
 print(f"Valeurs lues      : {len(values)}")
