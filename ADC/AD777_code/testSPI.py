1581ef27d5beb03cb18f36020aa1b37cf8c56059
BenoitSchick <benoitschick19@gmail.com>
2026-05-22 11:25:55 +0200

    Update testSPI.py

--- ADC/AD777_code/testSPI.py
+++ ADC/AD777_code/testSPI.py
@@ -1,51 +1,37 @@
 import spidev
-import time
 import RPi.GPIO as GPIO
+import time
 
-# =========================
-# Configuration GPIO
-# =========================
-
-CS_PIN = 17   # GPIO17
+CS = 0
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(CS_PIN, GPIO.OUT)
+GPIO.setup(CS, GPIO.OUT)
 
-GPIO.output(CS_PIN, 1)
-
-# =========================
-# Configuration SPI
-# =========================
+GPIO.output(CS, 1)
 
 spi = spidev.SpiDev()
-
-# bus 0, device 0 = /dev/spidev0.0
 spi.open(0, 0)
 
-spi.max_speed_hz = 10000   # 10 kHz
-spi.mode = 0               # CPOL=0 CPHA=0
-spi.bits_per_word = 8
+spi.no_cs = True      # IMPORTANT
+spi.mode = 0
+spi.max_speed_hz = 10000
 
-print("=== SPI Scope Test Raspberry Pi ===")
+try:
+    while True:
 
-while True:
+        GPIO.output(CS, 0)
 
-    GPIO.output(CS_PIN, 0)
+        spi.xfer2([0xAA])
+        time.sleep(0.05)
 
-    spi.xfer2([0xAA])
-    time.sleep(0.05)
+        spi.xfer2([0x55])
+        time.sleep(0.05)
 
-    spi.xfer2([0x55])
-    time.sleep(0.05)
+        GPIO.output(CS, 1)
 
-    spi.xfer2([0xFF])
-    time.sleep(0.05)
+        time.sleep(1)
 
-    spi.xfer2([0x00])
-    time.sleep(0.05)
-
-    GPIO.output(CS_PIN, 1)
-
-    print("Trame envoyée")
-
-    time.sleep(1)
+finally:
+    GPIO.output(CS, 1)
+    GPIO.cleanup()
+    spi.close()
